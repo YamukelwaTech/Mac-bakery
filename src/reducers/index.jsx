ccf44be24f46658eded4b19771383fdd676fd460
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-27 08:35:30 +0200

    removing files

--- src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-// Import your reducer functions here
-
-const rootReducer = combineReducers({
-  // Add your reducer functions here
-});
-
-export default rootReducer;
--- src/store.jsx
+++
@@ -1,7 +0,0 @@
-
-import { createStore } from 'redux';
-import rootReducer from './reducers'; // Assuming you have a rootReducer defined
-
-const store = createStore(rootReducer); // Create Redux store with rootReducer
-
-export default store;

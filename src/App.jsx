345634299c1322c07cb7f59ddedf30f2733c68e2
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-27 18:53:53 +0200

    tailwind fix . finally working

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MyRoutes from "./Routes";
 
 const App = () => {
   return (
-    <div className="bg-pink-500">
+    <div className="bg-yellow-500">
       <Navigation />
       <MyRoutes />
     </div>

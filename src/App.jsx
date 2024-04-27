81d0ea2ebe0af656b8ff71387c4876439a48ea3c
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-27 09:38:50 +0200

    fixed configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./App.css"
 
 const App = ({ count, increment, decrement }) => {
   return (
-    <div>
+    <div className="body">
       <h1>Count: {count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
